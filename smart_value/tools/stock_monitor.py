f5aa6553d352a02f7d5ea583a58ae2b3b36fd016
jerry <jerrychen.works@gmail.com>
2024-10-28 18:28:45 +0800

    Updated the monitor

--- smart_value/tools/stock_monitor.py
+++ smart_value/tools/stock_monitor.py
@@ -98,7 +98,7 @@ def update_opportunities(s_monitor, op_list):
         monitor_sheet.range((r, 20)).value = op.non_controlling_interests
         monitor_sheet.range((r, 21)).value = op.pre_tax_profit
         # Price Alert
-        monitor_sheet.range((r, 22)).value = f'=IF(D{r}<=H{r},"Y","")'
+        monitor_sheet.range((r, 22)).value = f'=IF(D{r}<=I{r},"Y","")'
         r += 1
     print(f"Total {len(op_list)} opportunities Updated")
 
