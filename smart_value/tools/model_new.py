39a2aa1e827b807883d61c8d0cfd901117abc429
Jerry Chen <Jerrychen.works@gmail.com>
2024-12-07 16:46:53 +0800

    added model_new function

--- smart_value/tools/model_new.py
+++ smart_value/tools/model_new.py
@@ -1,14 +1,18 @@
 import os
+from datetime import date
 import xlwings
+
+from smart_value.tools import model_dash
 from smart_value.tools.find_docs import get_template_paths, new_latest_model
 from smart_value.tools.model_dash import model_pos
 from smart_value.data import yq_data as yq
 
 
-def new_stock_model(ticker):
+def new_stock_model(ticker, comp_group):
     """Creates a new model if it doesn't already exist, then update.
 
     :param ticker: the string ticker of the stock
+    :param comp_group: the string identifier of the stock comparable group ID
     """
 
     template_path_list = get_template_paths()
@@ -17,13 +21,14 @@ def new_stock_model(ticker):
     model_name = ticker + "_" + os.path.basename(template_path_list[0])
     model_path = new_latest_model(model_name)
     # update the model
-    update_new_model(ticker, model_name, model_path)
+    update_new_model(ticker, comp_group, model_name, model_path)
 
 
-def update_new_model(ticker, model_name, model_path):
+def update_new_model(ticker, comp_group, model_name, model_path):
     """Update the model.
 
     :param ticker: the string ticker of the stock
+    :param comp_group: the string identifier of the stock comparable group ID
     :param model_name: the model file name
     :param model_path: the model file path
     """
@@ -33,18 +38,55 @@ def update_new_model(ticker, model_name, model_path):
 
     with xlwings.App(visible=False) as app:
         model_xl = app.books.open(model_path)
-        new_inputs(model_xl.sheets('Inputs'), company)
+        new_inputs(model_xl.sheets('Inputs'), company, comp_group)
+        new_dash(model_xl.sheets('Dashboard'), company)
         model_xl.save(model_path)
         model_xl.close()
 
 
-def new_inputs(model_xl, company):
+def new_inputs(input_sheet, company, comp_group):
     """Get the new inputs
 
-    :param model_xl: the Excel file of the model
+    :param input_sheet: the Excel file of the model
     :param company: the YqStock object of the stock
+    :param comp_group: the string identifier of the stock comparable group ID
     """
 
-    input_sheet = model_xl.sheets('Inputs')
-    dash_sheet = model_xl.sheets('Dashboard')
-    pass
+    data_digits = len(str(int(company.is_df.iloc[0, 0])))
+    # print(str(int(stock.is_df.iloc[0, 0])))
+    # print(data_digits)
+    if 13 > data_digits >= 7:
+        report_unit = 1000
+    elif data_digits >= 13:
+        report_unit = 1000000
+    else:
+        report_unit = 1
+
+    input_sheet.range('C4').value = company.symbol
+    input_sheet.range('C5').value = company.name
+    input_sheet.range('C6').value = date.today
+    input_sheet.range('C9').value = comp_group
+    input_sheet.range('C10').value = company.shares
+    input_sheet.range('C11').value = company.report_currency
+    input_sheet.range('C12').value = company.last_fy
+    input_sheet.range('C13').value = report_unit
+
+    for i in range(len(company.is_df.columns)):
+        # income statement
+        input_sheet.range((25, i + 3)).value = int(company.is_df.iloc[0, i] / report_unit)  # Sales
+        input_sheet.range((26, i + 3)).value = int(company.is_df.iloc[1, i] / report_unit)  # COGS
+        input_sheet.range((27, i + 3)).value = int(company.is_df.iloc[2, i] / report_unit)  # Operating Expenses
+        input_sheet.range((29, i + 3)).value = int(company.is_df.iloc[3, i] / report_unit)  # Interest Expense
+
+
+def new_dash(dash_sheet, company):
+    """Update the Data sheet.
+
+    :param dash_sheet: the xlwings object of Data
+    :param company: the Stock object
+    """
+
+    dash_sheet.range('G3').value = company.price
+    dash_sheet.range('H3').value = company.price_currency
+    dash_sheet.range('G7').value = company.fx_rate
+    model_dash.update_dash_marco(dash_sheet)
