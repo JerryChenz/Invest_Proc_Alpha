98d7d9376988856f45df8a88eed01e34900177f0
Jerry Chen <Jerrychen.works@gmail.com>
2024-12-07 22:35:04 +0800

    added model_new function

--- invest_proc_main.py
+++ invest_proc_main.py
@@ -1,6 +1,4 @@
 from smart_value.tools.control_ui import invest_proc
-from smart_value.tools.model_new import new_stock_model
 
 if __name__ == '__main__':
     invest_proc()
-    # new_stock_model("3606.HK", "test")
