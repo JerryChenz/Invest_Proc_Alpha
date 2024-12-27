409ce05ad58488e2e98b8cbf6192903f47fadd8d
jerry <jerrychen.works@gmail.com>
2024-12-27 18:33:29 +0800

    Updated the Valuations

--- smart_value/tools/helper.py
+++ smart_value/tools/helper.py
@@ -12,9 +12,14 @@ def copy_value():
         with xlwings.App(visible=False) as app:
             xl_model = app.books.open(p)
             input_sheet = xl_model.sheets('Inputs')
-            fin_sheet = xl_model.sheets('Fin_Analysis')
-            input_sheet.range("C41").value = fin_sheet.range("D3").value
-            input_sheet.range("C42").value = fin_sheet.range("D4").value
-            input_sheet.range("C37").value = fin_sheet.range("I49").value
+            # Case 1
+            input_sheet.range("C30:M33").copy()
+            input_sheet.range("C31:M34").paste(paste="formulas")
+            input_sheet.range("C30:M30").clear_contents()
+            # Case 2
+            # fin_sheet = xl_model.sheets('Fin_Analysis')
+            # input_sheet.range("C41").value = fin_sheet.range("D3").value
+            # input_sheet.range("C42").value = fin_sheet.range("D4").value
+            # input_sheet.range("C37").value = fin_sheet.range("I49").value
             xl_model.save(p)
             xl_model.close()
